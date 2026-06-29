d9437aaab491a8afe3633c03dc1fee70ca62aceb
Chaya Teitelbaum <Chay736382@hotmail.com>
2026-06-28 20:01:58 -0400

    Implement landing page design based on holixpage mock

--- Holix/obj/Debug/net10.0/Holix.AssemblyInfo.cs
+++ Holix/obj/Debug/net10.0/Holix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Holix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1fcae09c56987e7c3124fa2c61e0aa52d775f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Holix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Holix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
